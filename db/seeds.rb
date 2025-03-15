cd23058d342c83bf8fc003805862c67bc3fe1db2
Javier Blanco de Torres <jblanco@venped.com>
2025-03-15 08:52:59 +0700

    Timezone

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,25 +2,3 @@ User.create(email: "alex@danang365.com", password: "123123123", alias_code: "DAN
 User.create(email: "afc@furamavietnam.com", password: "123123123", alias_code: "FUR", admin: true)
 User.create(email: "patakkadanang@gmail.com", password: "123123123", alias_code: "PAT", admin: true)
 User.create(email: "tickets@danang365.com", password: "123123123", alias_code: "TIC")
-
-
-# 2000.times do |i|
-#   status = Entry.statuses.keys.sample
-#   entry_type = Entry.entry_types.keys.sample
-
-#   entry_attributes = {
-#     entry_type: entry_type,
-#     user: User.all.sample,
-#     status: status
-#   }
-
-#   if status != 'created' 
-#     entry_attributes[:name] = Faker::Movies::StarWars.character
-#     entry_attributes[:phone] = Faker::PhoneNumber.phone_number
-#     entry_attributes[:email] = Faker::Internet.email
-#   end
-
-#   Entry.create(entry_attributes)
-
-#   puts "#{i} "
-# end
